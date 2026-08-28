26bcef8abdf140b8089b60cfdb8587ca7f255dd6
karsont <karsont@uci.edu>
2026-08-28 12:45:05 -0700

    attempt bermuda rectangle problem

---
+++ my_solutions/bermuda_rectangle.py
@@ -0,0 +1,35 @@
+def solution(problemInput: list):
+    res = 0
+
+    x, y = problemInput[0]
+
+    for i in range(1, len(problemInput)):
+        k, m = problemInput[i]
+        print(min(k, x) * min(m, y))
+
+def formatTestCase():
+    bermudaRectangle = input()
+    x, y = bermudaRectangle.split()
+    x = int(x)
+    y = int(y)
+
+    res = [(x, y)]
+
+    for _ in range(y):
+        testArea = input()
+        k, m = testArea.split()
+        k = int(k)
+        m = int(m)
+
+        res.append((k, m))
+
+    return res
+
+def main():
+    numOfTestCases = int(input())
+    for _ in range(numOfTestCases):
+        solInput = formatTestCase()
+        solution(solInput)
+
+if __name__ == '__main__':
+    main()
